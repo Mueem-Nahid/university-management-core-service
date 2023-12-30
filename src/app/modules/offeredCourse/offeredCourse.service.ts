121b711461776295c0aecf8f0174a99fc25a8961
Mueem-Nahid <mueem51@gmail.com>
2023-12-31 00:20:29 +0600

    # Info - add validation while creating offered courses

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -1,6 +1,8 @@
 import { ICreateOfferedCourse } from "./offeredCourse.interface";
 import { OfferedCourse } from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const createOfferedCourse = async (data: ICreateOfferedCourse): Promise<OfferedCourse[]> => {
   const {academicDepartmentId, semesterRegistrationId, courseIds} = data;
@@ -10,9 +12,29 @@ const createOfferedCourse = async (data: ICreateOfferedCourse): Promise<OfferedC
     courseId,
   }));
 
+  // Check for existing courses
+  const existingCourses = await prisma.offeredCourse.findMany({
+    where: {
+      academicDepartmentId,
+      semesterRegistrationId,
+      courseId: {
+        in: courseIds,
+      },
+    },
+  });
+
+  // Extract the existing course IDs
+  const existingCourseIds = new Set(existingCourses.map((course) => course.courseId));
+
+  // Filter out courses that already exist
+  const newCoursesData = offeredCoursesData.filter((courseData) => !existingCourseIds.has(courseData.courseId));
+
+  if (!newCoursesData?.length)
+    throw new ApiError(httpStatus.BAD_REQUEST, "Course already exist.")
+
   // bulk insert
   await prisma.offeredCourse.createMany({
-    data: offeredCoursesData,
+    data: newCoursesData,
   });
 
   // Fetch the created courses
@@ -24,6 +46,11 @@ const createOfferedCourse = async (data: ICreateOfferedCourse): Promise<OfferedC
         in: courseIds,
       },
     },
+    include: {
+      academicDepartment: true,
+      semesterRegistration: true,
+      course: true
+    }
   });
 
   return createdCourses;
