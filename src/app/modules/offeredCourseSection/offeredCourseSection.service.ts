f7bdb5012a6279f90bbaf44949e8706e78e847d8
Mueem-Nahid <mueem51@gmail.com>
2023-12-31 23:29:24 +0600

    # Info - add validation

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -1,7 +1,19 @@
 import { OfferedCourseSection } from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const createOfferedCourseSection = async (data: OfferedCourseSection): Promise<OfferedCourseSection> => {
+  const isOfferedCourseExist = await prisma.offeredCourse.findFirst({
+    where: {
+      id: data.offeredCourseId
+    }
+  });
+  if (!isOfferedCourseExist)
+    throw new ApiError(httpStatus.BAD_REQUEST, "Offered course does not exist.");
+
+  data.semesterRegistrationId = isOfferedCourseExist.semesterRegistrationId;
+
   const result = await prisma.offeredCourseSection.create({
     data
   });
--- todo/todo.md
+++ todo/todo.md
@@ -1,12 +1,8 @@
 1. Add roles to the routes in the following modules:
-   - Academic Department
-   - Academic Faculty
-   - Faculty
-   - Room
-   - Course
-2. Intrgrate get data by id, filter data, update and delete functionalities in the following modules:
-   - semester registration - filter, get one by id, update , delete
+   - 
+2. Integrate get data by id, filter data, update and delete functionalities in the following modules:
+   - offered course - filter, get one by id, update , delete
 3. Add Zod validations and role in the following modules:
-   - semester registration
+   - 
 
-- start from 38-10
+- start from 39.1
