70b17c1f6a48ce1e64a6eb861aed04ba75dc24ae
Mueem-Nahid <mueem51@gmail.com>
2024-01-01 21:13:44 +0600

    # Info - get by id, get all, update , delete offered course section

--- src/app/modules/offeredCourseSection/offeredCourseSection.controller.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.controller.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 import { OfferedCourseSectionService } from "./offeredCourseSection.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { offeredCourseSectionFilterableFields } from "./offeredCourseSection.constant";
+import pick from "../../../shared/pick";
 
 const createOfferedCourseSection = catchAsync(async (req: Request, res: Response) => {
   const result = await OfferedCourseSectionService.createOfferedCourseSection(req.body);
@@ -14,6 +16,56 @@ const createOfferedCourseSection = catchAsync(async (req: Request, res: Response
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, offeredCourseSectionFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await OfferedCourseSectionService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourseSections fetched successfully',
+    meta: result.meta,
+    data: result.data
+  });
+})
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const result = await OfferedCourseSectionService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourseSection fetched successfully',
+    data: result
+  });
+})
+
+const updateOneInDB = catchAsync(async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const result = await OfferedCourseSectionService.updateOneInDB(id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourseSection updated successfully',
+    data: result
+  });
+})
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const {id} = req.params;
+  const result = await OfferedCourseSectionService.deleteByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'OfferedCourseSection deleted successfully',
+    data: result
+  });
+})
+
 export const OfferedCourseSectionController = {
-  createOfferedCourseSection
+  createOfferedCourseSection,
+  getAllFromDB,
+  getByIdFromDB,
+  updateOneInDB,
+  deleteByIdFromDB
 }
--- src/app/modules/offeredCourseSection/offeredCourseSection.route.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.route.ts
@@ -1,8 +1,32 @@
 import express from "express";
 import { OfferedCourseSectionController } from "./offeredCourseSection.controller";
+import { ENUM_USER_ROLE } from "../../../enums/user";
+import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
+import { OfferedCourseSectionValidation } from "./offeredCourseSection.validation";
 
 const router = express.Router();
 
-router.post('/', OfferedCourseSectionController.createOfferedCourseSection);
+router.post('/',
+  validateRequest(OfferedCourseSectionValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseSectionController.createOfferedCourseSection);
+
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseSectionValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseSectionController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  OfferedCourseSectionController.deleteByIdFromDB
+);
+
+router.get('/', OfferedCourseSectionController.getAllFromDB);
+
+router.get('/:id', OfferedCourseSectionController.getByIdFromDB);
 
 export const OfferedCourseSectionRoutes = router;
