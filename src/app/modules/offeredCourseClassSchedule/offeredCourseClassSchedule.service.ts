599551dab2ba7b8565768e09c905789da270243d
mueem51@gmail.com <mueem51@gmail.com>
2024-01-03 23:50:29 +0600

    # Info - check booked room

--- src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.service.ts
@@ -1,4 +1,5 @@
 import { OfferedCourseClassSchedule } from "@prisma/client";
+import ApiError from "../../../errors/ApiError";
 import prisma from "../../../shared/prisma";
 
 const createClassSchedule = async (data: OfferedCourseClassSchedule): Promise<OfferedCourseClassSchedule> => {
@@ -23,24 +24,27 @@ const createClassSchedule = async (data: OfferedCourseClassSchedule): Promise<Of
   }
 
   for (const slot of alreadyBookedRoom) {
-    const formattedExistingStartTime = new Date(`1970-01-01${slot.startTime}:00`);
-    const formattedExistingEndTime = new Date(`1970-01-01${slot.startTime}:00`);
-    const formattedNewStartTime = new Date(`1970-01-01${newSlot.startTime}:00`);
-    const formattedNewEndTime = new Date(`1970-01-01${newSlot.startTime}:00`);
+    const formattedExistingStartTime = new Date(`1970-01-01T${slot.startTime}:00`);
+    const formattedExistingEndTime = new Date(`1970-01-01T${slot.startTime}:00`);
+    const formattedNewStartTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
+    const formattedNewEndTime = new Date(`1970-01-01T${newSlot.startTime}:00`);
+  
+    if(formattedNewStartTime < formattedExistingEndTime && formattedNewEndTime > formattedExistingStartTime){
+      throw new ApiError(httpStatus.CONFLICT, "Room is already booked.");
+    }
   }
 
-  console.log(alreadyBookedRoom)
 
-  // const result = await prisma.offeredCourseClassSchedule.create({
-  //   data,
-  //   include: {
-  //     semesterRegistration: true,
-  //     offeredCourseSection: true,
-  //     room: true,
-  //     faculty: true
-  //   }
-  // });
-  // return result;
+  const result = await prisma.offeredCourseClassSchedule.create({
+    data,
+    include: {
+      semesterRegistration: true,
+      offeredCourseSection: true,
+      room: true,
+      faculty: true
+    }
+  });
+  return result;
 }
 
 export const OfferedCourseClassScheduleService = {
